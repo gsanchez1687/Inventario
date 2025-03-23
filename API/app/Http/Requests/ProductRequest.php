ab2729613cd1855dd35af7407c4bf3606a88cf15
Guillermo <guillermo@MacBook-Pro-de-Guillermo.local>
2025-03-23 06:20:50 -0400

    ADD:: Pint

--- API/app/Http/Requests/ProductRequest.php
+++ API/app/Http/Requests/ProductRequest.php
@@ -5,7 +5,6 @@ namespace App\Http\Requests;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
-use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpFoundation\Response;
 
 class ProductRequest extends FormRequest
