1fe2ea440f4e44bf79363145bef28396561e2fe1
Guillermo <guillermo@MacBook-Pro-de-Guillermo.local>
2025-03-24 00:22:42 -0400

    ADD:: admin

---
+++ admin/nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+        link: [
+            { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css' }
+        ],
+        script: [
+            { src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js' }
+        ]
+    }
+},
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    '@nuxt/scripts'
+  ],
+})
